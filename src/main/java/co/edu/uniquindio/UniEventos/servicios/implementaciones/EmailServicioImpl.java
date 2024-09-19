7d58bfb0c35c79e0014fece1b57c58a539693a33
DanielaLosada <danielai.losadar@uqvirtual.edu.co>
2024-09-18 19:50:35 -0500

    Se añaden dependecias de correo electronico, firebase y mercadoPago

--- src/main/java/co/edu/uniquindio/UniEventos/servicios/implementaciones/EmailServicioImpl.java
+++ src/main/java/co/edu/uniquindio/UniEventos/servicios/implementaciones/EmailServicioImpl.java
@@ -2,28 +2,33 @@ package co.edu.uniquindio.UniEventos.servicios.implementaciones;
 
 import co.edu.uniquindio.UniEventos.dto.EmailDTO.EmailDTO;
 import co.edu.uniquindio.UniEventos.servicios.interfaces.EmailServicio;
+import org.simplejavamail.api.email.Email;
+import org.simplejavamail.api.mailer.Mailer;
+import org.simplejavamail.api.mailer.config.TransportStrategy;
+import org.simplejavamail.email.EmailBuilder;
+import org.simplejavamail.mailer.MailerBuilder;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
-
+import static org.simplejavamail.config.ConfigLoader.Property.SMTP_PORT;
 @Service
 public class EmailServicioImpl implements EmailServicio {
-    @Autowired
-    private JWTUtils jwtUtils;
-    private final JavaMailSender javaMailSender;
-    /**
-     * envio de correo normal puede servir para notificaciones
-     *
-     * @param emailDTO
-     * @throws Exception
-     */
     @Override
+    @Async
     public void enviarCorreo(EmailDTO emailDTO) throws Exception {
-        MimeMessage mensaje = javaMailSender.createMimeMessage();
-        MimeMessageHelper helper = new MimeMessageHelper(mensaje);
-        helper.setSubject(emailDTO.asunto());
-        helper.setText(emailDTO.cuerpo(), true);
-        helper.setTo(emailDTO.destinatario());
-        helper.setFrom("no_reply@dominio.com");
-        javaMailSender.send(mensaje);
+        Email email = EmailBuilder.startingBlank()
+                .from("SMTP_USERNAME")
+                .to(emailDTO.destinatario())
+                .withSubject(emailDTO.asunto())
+                .withPlainText(emailDTO.cuerpo())
+                .buildEmail();
+
+        try (Mailer mailer = MailerBuilder
+                .withSMTPServer("SMTP_HOST", SMTP_PORT.ordinal(), "SMTP_USERNAME", "SMTP_PASSWORD")
+                .withTransportStrategy(TransportStrategy.SMTP_TLS)
+                .withDebugLogging(true)
+                .buildMailer()) {
+            mailer.sendMail(email);
+        }
     }
 }
--- src/main/java/co/edu/uniquindio/UniEventos/servicios/implementaciones/ImagenesServicioImpl.java
+++ src/main/java/co/edu/uniquindio/UniEventos/servicios/implementaciones/ImagenesServicioImpl.java
@@ -1,34 +1,29 @@
 package co.edu.uniquindio.UniEventos.servicios.implementaciones;
 
 import co.edu.uniquindio.UniEventos.servicios.interfaces.ImagenesServicio;
+import com.google.cloud.storage.*;
+import com.google.firebase.cloud.StorageClient;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+import java.util.UUID;
 
 @Service
 public class ImagenesServicioImpl implements ImagenesServicio {
-
-    private final Cloudinary cloudinary;
-    public ImagenesServicioImpl(){
-        Map<String, String> config = new HashMap<>();
-        config.put("cloud_name", "dk0vyi7yi");
-        config.put("api_key", "444834788181461");
-        config.put("api_secret", "bMFA0ikjKLLkZAc-M5OqbLdOcd4");
-        cloudinary = new Cloudinary(config);
+    @Override
+    public String subirImagen(MultipartFile multipartFile) throws Exception{
+        Bucket bucket = StorageClient.getInstance().bucket();
+        String fileName = String.format( "%s-%s", UUID.randomUUID().toString(), multipartFile.getOriginalFilename() );
+        Blob blob = bucket.create( fileName, multipartFile.getInputStream(), multipartFile.getContentType() );
+        return String.format(
+                "https://firebasestorage.googleapis.com/v0/b/%s/o/%s?alt=media",
+                bucket.getName(),
+                blob.getName()
+        );
     }
     @Override
-    public Map subiImagen(MultipartFile imagen) throws Exception {
-        File file = convertir(imagen);
-        return cloudinary.uploader().upload(file, ObjectUtils.asMap("folder", "UniLocal"));
-    }
-
-    @Override
-    public Map eliminarImagen(String idImagen) throws Exception {
-        return cloudinary.uploader().destroy(idImagen, ObjectUtils.emptyMap());
-    }
-    private File convertir(MultipartFile imagen) throws IOException {
-        File file = File.createTempFile(imagen.getOriginalFilename(), null);
-        FileOutputStream fos = new FileOutputStream(file);
-        fos.write(imagen.getBytes());
-        fos.close();
-        return file;
+    public void eliminarImagen(String nombreImagen) throws Exception{
+        Bucket bucket = StorageClient.getInstance().bucket();
+        Blob blob = bucket.get(nombreImagen);
+        blob.delete();
     }
 }
